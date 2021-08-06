b44214c981a549464413bf1825f5644d5f812782
bek <rakhimov.bek20@gmail.com>
2021-08-06 10:38:46 -0400

    initial push

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+
+namespace text_to_speech.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task OnPost()
+        {
+            var text = Request.Form["text"];
+            await SynthesizeAudioAsync(text);
+        }
+
+
+        static async Task SynthesizeAudioAsync(string text)
+        {
+            var config = SpeechConfig.FromSubscription("< your token >", "< location >");
+            using var synthesizer = new SpeechSynthesizer(config);
+            await synthesizer.SpeakTextAsync(text);
+        }
+    }
+}
